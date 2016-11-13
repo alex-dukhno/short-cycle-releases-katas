77b2e992b45e42ef4034c217c7566c80490f8070
Alex-Diez <alexvic.amatory@gmail.com>
2016-11-13 17:51:35 +0200

    calculator evaluates addition

--- src/main/java/calc/Calculator.java
+++ src/main/java/calc/Calculator.java
@@ -16,6 +16,14 @@ public class Calculator {
     }
 
     public int evaluate(String expression) {
-        return Integer.parseInt(expression);
+        int index = 0;
+        while (index < expression.length() && expression.charAt(index) != '+') {
+            index += 1;
+        }
+        int result = Integer.parseInt(expression.substring(0, index));
+        if (index < expression.length()) {
+            result += Integer.parseInt(expression.substring(index + 1));
+        }
+        return result;
     }
 }
